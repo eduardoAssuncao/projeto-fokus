983592706a4b5c105a6d98718f3ff30b3903b844
Eduardo (Kenny8855) Assunção <39742355+eduardoAssuncao@users.noreply.github.com>
2024-04-24 11:35:00 -0300

    Add files via upload

---
+++ Fokus/script.js
@@ -0,0 +1,121 @@
+const html = document.querySelector('html');
+const focoBt = document.querySelector('.app__card-button--foco');
+const curtoBt = document.querySelector('.app__card-button--curto');
+const longoBt = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause');
+const startPauseBtIcon = document.querySelector('#start-pause img');
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
+const tempoNaTela = document.querySelector('#timer');
+
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const playSound = new Audio('/sons/play.wav');
+const pauseSound = new Audio('/sons/pause.mp3');
+const finishSound = new Audio('/sons/beep.mp3');
+let tempoDecorridoEmSegundos = 1500;
+let intervaloId = null;
+
+musica.loop = true;
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused){
+        musica.play();
+    }else {
+        musica.pause();
+    }
+})
+
+focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500;
+    alterarContexto('foco');
+    focoBt.classList.add('active');
+    //verificar a probalidade de reiniciar a música
+})
+
+curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300;
+    alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
+})
+
+longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900;
+    alterarContexto('descanso-longo');
+    longoBt.classList.add('active');
+})
+
+function alterarContexto(contexto){
+    mostrarTempo();
+    botoes.forEach((contexto) => {
+        contexto.classList.remove('active');
+    });
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `/imagens/${contexto}.png`);
+    switch (contexto){
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+            Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta.</strong>
+            `
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+            Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>
+            `
+            break;
+        default:
+            break;
+    }
+}
+
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0){
+        zerar();
+        finishSound.play();
+        return;
+    }
+    tempoDecorridoEmSegundos -= 1;
+    mostrarTempo();
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar() {
+    if(intervaloId){
+        pauseSound.play();
+        startPauseBtIcon.setAttribute('src', 'imagens/play_arrow.png');
+        zerar();
+        return
+    }
+    playSound.play();
+    startPauseBtIcon.setAttribute('src', 'imagens/pause.png');
+    intervaloId = setInterval(contagemRegressiva, 1000);
+    iniciarOuPausarBt.textContent = "Pausar";
+}
+
+function zerar() {
+    if(tempoDecorridoEmSegundos <= 0){
+        tempoDecorridoEmSegundos = 1500;
+    }
+    clearInterval(intervaloId);
+    iniciarOuPausarBt.textContent = "Começar";
+    intervaloId = null;
+}
+
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'});
+    tempoNaTela.innerHTML = `${tempoFormatado}`;
+}
+
+mostrarTempo();
